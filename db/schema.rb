aa8681716bcf942530c42b83cd8b4bce60f9ca67
Steven Magadan <steven.magadan@hotmail.com>
2016-02-25 15:18:32 -0800

    changed databases for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160224201726) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "answers", force: :cascade do |t|
     t.integer  "question_id"
     t.string   "title"
@@ -29,7 +32,7 @@ ActiveRecord::Schema.define(version: 20160224201726) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "questions", ["user_id"], name: "index_questions_on_user_id"
+  add_index "questions", ["user_id"], name: "index_questions_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
@@ -38,4 +41,5 @@ ActiveRecord::Schema.define(version: 20160224201726) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "questions", "users"
 end
